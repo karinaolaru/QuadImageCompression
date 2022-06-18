6dd97bb99be2067485d314fa575dee40956dc942
karinaolaru <karinaolaru26@gmail.com>
2022-06-18 15:32:20 +0300

    Add chrono functionality

--- CompresieImagine/CompresieImagine.cpp
+++ CompresieImagine/CompresieImagine.cpp
@@ -1,13 +1,33 @@
 #include <iostream>
 #include <fstream>
 #include "Image.h"
+#include <chrono>
+#include <ctime>
+#include <iomanip>
 
 int main(){
-    Image image("D:\\FACULTATE\\ANUL I\\SEMESTRUL II\\Structuri de date\\CompresieImagine\\blue_jay_256x256.jpg");
+    std::chrono::time_point<std::chrono::system_clock> start, end; 
+
+    start = std::chrono::system_clock::now();
+    Image image("D:\\FACULTATE\\ANUL I\\SEMESTRUL II\\Structuri de date\\CompresieImagine\\hibi_1024x1024.png");
+    end = std::chrono::system_clock::now();
+
+    std::chrono::duration<double> elapsed_seconds = end - start;
+    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+
+    std::cout << "Image loading took: " <<std::setprecision(3)<< elapsed_seconds.count() << "s\n";
 
     for (int i = 0; i <= image.getMaxLevel(); i++) {
+        start = std::chrono::system_clock::now();
         Image* com = image.compress(i);
-        std::string path = std::string("D:\\FACULTATE\\ANUL I\\SEMESTRUL II\\Structuri de date\\CompresieImagine\\BlueJayCompressed\\test") + std::to_string(i) + std::string(".png");
+        end = std::chrono::system_clock::now();
+
+        elapsed_seconds = end - start;
+        end_time = std::chrono::system_clock::to_time_t(end);
+
+        std::cout << "Image compression " <<i<<" took: "<< std::setprecision(3) << elapsed_seconds.count() << "s\n";
+
+        std::string path = std::string("D:\\FACULTATE\\ANUL I\\SEMESTRUL II\\Structuri de date\\CompresieImagine\\HibiCompressed\\test") + std::to_string(i) + std::string(".png");
         com->save(path);
         delete com;
     }
