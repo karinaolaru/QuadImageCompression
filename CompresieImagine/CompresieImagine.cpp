c42e2c3f25cc9a7dc392ddb667934d02827c5195
karinaolaru <karinaolaru26@gmail.com>
2022-06-18 13:20:20 +0300

    CompressionTest

--- CompresieImagine/CompresieImagine.cpp
+++ CompresieImagine/CompresieImagine.cpp
@@ -3,11 +3,11 @@
 #include "Image.h"
 
 int main(){
-    Image image("D:\\FACULTATE\\ANUL I\\SEMESTRUL II\\Structuri de date\\CompresieImagine\\hibi_512x512.png");
+    Image image("D:\\FACULTATE\\ANUL I\\SEMESTRUL II\\Structuri de date\\CompresieImagine\\blue_jay_256x256.jpg");
 
     for (int i = 0; i <= image.getMaxLevel(); i++) {
         Image* com = image.compress(i);
-        std::string path = std::string("D:\\FACULTATE\\ANUL I\\SEMESTRUL II\\Structuri de date\\CompresieImagine\\HibiCompressed\\test") + std::to_string(i) + std::string(".png");
+        std::string path = std::string("D:\\FACULTATE\\ANUL I\\SEMESTRUL II\\Structuri de date\\CompresieImagine\\BlueJayCompressed\\test") + std::to_string(i) + std::string(".png");
         com->save(path);
         delete com;
     }
